b7ea0acd8079eba89f23430f4561a86a0da922e8
Zarina <zaarina97@gmail.com>
2019-09-04 19:36:22 +0200

    oppg1

--- src/oblig1.java
+++ src/oblig1.java
@@ -4,6 +4,8 @@
     Zarina Iljasova - S311806
  */
 
+import java.util.NoSuchElementException;
+
 public class oblig1 {
 
     public static int maks(int[] a){
@@ -27,9 +29,29 @@ public class oblig1 {
     }
 
     public static void main(String[] args){
-        int [] a = {1,5,3,2,6,8,1};
+        int [] a = {1,5,3,2,6,8,1,9,10};
 
         System.out.println(oblig1.maks(a));
+        System.out.println(oblig1.ombyttinger(a));
+    }
+
+
+    public static int ombyttinger(int[] a) {
+
+
+        int ombytt = 0;
+        if (a.length < 1)
+            throw new NoSuchElementException("Tabellen er tom");
+
+        for (int i = 1; i < a.length - 1; i++) {
+            if (a[i + 1] > a[i]) {
+                int maksVerdi = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = maksVerdi;
+                ombytt++;
+            }
+        }
+        return ombytt;
     }
 }
 
