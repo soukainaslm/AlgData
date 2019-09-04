5b9e87209164deff646b33e51c9db5a4bddc5bdc
soukainaslm <soukainaslm@gmail.com>
2019-09-04 18:38:01 +0200

    oppgave1

--- src/oblig1.java
+++ src/oblig1.java
@@ -1,5 +1,24 @@
 public class oblig1 {
-    public static void main(String[] args){
-        System.out.println("grd");
-    }
+    public static int maks(int[] a) // a er en heltallstabell
+    {
+        if (a.length < 1) {
+
+
+         throw new java.util.NoSuchElementException("Tabellen a er tom!");
+        }
+
+        int m = 0;  // indeks til foreløpig største verdi
+
+        for (int i = 1; i < a.length; i++)  // obs: starter med i = 1
+        {
+
+            if (a[i] > a[m]) m = i;  // indeksen oppdateres
+
+        }
+
+        return m;  // returnerer indeksen/posisjonen til største verdi
+
+    }  // maks
+
 }
+
